36187d290503416a7065f0859d45a456207a92a4
Yevgen <kolesnikov1987ua@gmil.com>
2021-01-26 18:27:44 +0200

    добавили показ модальных окон, выполнили пункты ТЗ (2, 3, 5,13, 14)

---
+++ src/js/modules/modals.js
@@ -0,0 +1,119 @@
+const modals = () => {
+    let btnPressed;
+
+    function bindModal(triggerSelector, modalSelector, closeSelector, destroy = false) {
+        const trigger = document.querySelectorAll(triggerSelector),
+              modal = document.querySelector(modalSelector),
+              close = document.querySelector(closeSelector),
+              windows = document.querySelectorAll('[data-modal]'),
+              scroll = calcScroll();
+
+        trigger.forEach(item => {
+            item.addEventListener('click', (e) => {
+                if (e.target) {
+                    e.preventDefault();
+                }
+
+                btnPressed = true;
+
+                if (destroy) {
+                    item.remove();
+                }
+
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                    item.classList.add('animated', 'fadeIn');
+                });
+
+                modal.style.display = 'block';
+                document.body.style.overflow = 'hidden';
+                document.body.style.marginRight = `${scroll}px`;
+            });
+        });
+
+        close.addEventListener('click', () => {
+            windows.forEach(item => {
+                item.style.display = 'none';
+            });
+
+            modal.style.display = 'none';
+            document.body.style.overflow = '';
+            document.body.style.marginRight = `0px`;
+        });
+
+        modal.addEventListener('click' , (e) => {
+            if (e.target === modal) {
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+
+                modal.style.display = 'none';
+                document.body.style.overflow = '';
+                document.body.style.marginRight = `0px`;
+            }
+        });
+    }
+
+    function showModalByTime(selector, time) {
+        setTimeout(() => {
+            let display;
+
+            document.querySelectorAll('[data-modal]').forEach(item => {
+                if (getComputedStyle(item).display !== 'none') {
+                    display = 'block';
+                }
+            });
+
+            if (!display) {
+                document.querySelector(selector).style.display = 'block';
+                document.body.style.overflow = 'hidden';
+                let scroll = calcScroll();
+                document.body.style.marginRight = `${scroll}px`;
+            }
+            
+        }, time);
+    }
+
+    function calcScroll() {
+        let div = document.createElement('div');
+
+        div.style.cssText = 'width: 50px; height: 50px; overflow-y: scroll; visibility: hidden';
+
+        document.body.appendChild(div);
+
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
+    }
+
+//открытие модального окна при прокрутки страницы до конца
+            //полная совместимость, стандартное поведение браузера (Standards Mode)
+    function openByScroll(selector) {
+        window.addEventListener('scroll', () => {
+            if (!btnPressed && (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight)) {
+                document.querySelector(selector).click();
+            }
+        })
+    }
+
+        // частичная совместимость браузеров или старые браузеры
+    // function openByScroll(selector) {
+    //     window.addEventListener('scroll', () => {
+    //         let scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
+
+    //         if (!btnPressed && (window.pageYOffset + document.documentElement.clientHeight >= scrollHeight)) {
+    //             document.querySelector(selector).click();
+    //         }
+    //     })
+    // }
+
+    bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
+    bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close'); 
+    bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true); 
+    openByScroll('.fixed-gift');
+
+    showModalByTime('.popup-consultation', 60000);
+};
+
+export default modals;
